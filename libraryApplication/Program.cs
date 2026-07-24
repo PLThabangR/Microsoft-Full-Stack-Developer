49b032bbe14c2c05ebc340a3dc793e1001e80f09
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2026-07-24 14:14:49 +0200

    >Net platform web appl;ications

---
+++ libraryApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Products
+{  
+    class Product
+    {
+        
+
+
+
+
+        Product( string name,double price){
+            name = name;
+            price = price;           
+        }
+
+        
+    }
+   class Program
+    {
+        
+    }
+    
+    }
+   
